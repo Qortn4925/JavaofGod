f83c8adae2932846a867e6c99ddabaef2cf9124c
qortn4925 <qortn4925@naver.com>
2024-08-14 17:41:00 +0900

    참조 자료형의 초기값과  선언방식 + 객체참조

---
+++ src/ch06/ArrayInitValue.java
@@ -0,0 +1,23 @@
+package ch06;
+
+public class ArrayInitValue {
+    public static void main(String[] args) {
+        ArrayInitValue ai = new ArrayInitValue();
+            ai.referenceTypes();
+    }
+    public void referenceTypes(){
+        //참조 자료형의 초기값 확인 >>  null
+
+
+        String [] St = new String[2];
+        St[0]= "aaaaaa";
+
+        ArrayInitValue[] ar =new ArrayInitValue[2];
+        ar[0] = new ArrayInitValue();
+        System.out.println("St  "+St[0]);
+        System.out.println("ar  "+ar[0]);     //>> 객체 출력 결과가 저모양인 이유는 참조 자료형은
+        
+//       toString()이라는 메소드를 오버라이딩 해야하는데  안해줘서 기본 형태임  파일위치.타입이름@ 고유번호
+        
+    }
+}
