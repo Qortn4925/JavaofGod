5aacaaebcaab10ef065ce6cd52611078fe99022c
qortn4925 <qortn4925@naver.com>
2024-08-20 16:58:00 +0900

    배열의 선언 방법과  초기화 ,   배열의 크기는 0~ n-1번 까지 존재

---
+++ src/ch07/ArrayLotto.java
@@ -0,0 +1,17 @@
+package ch07;
+
+public class ArrayLotto {
+    public static void main(String[] args) {
+        int[] lottoNumbers = new int[7];
+        lottoNumbers[0] =1;
+        lottoNumbers[1] =1;
+        lottoNumbers[2] =1;
+        lottoNumbers[3] =1;
+        lottoNumbers[4] =1;
+        lottoNumbers[5] =1;
+        lottoNumbers[6] =1;
+//        lottoNumbers[7] =1;
+        // 배열의 선언 방법과  초기화 ,   배열의 크기는 0~ n-1번 까지 존재
+    }
+    
+}
