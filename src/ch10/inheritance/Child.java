886acd928cb6f0cd40b10e0c6bb48a44cfe9d1b6
qortn4925 <qortn4925@naver.com>
2024-09-03 17:52:12 +0900

    상속  super

---
+++ src/ch10/inheritance/Child.java
@@ -0,0 +1,8 @@
+package ch10.inheritance;
+
+public class Child extends Parent {
+
+    public Child() {
+        System.out.println("자식 생성자");
+    }
+}
---
+++ src/ch10/inheritance/Parent.java
@@ -0,0 +1,12 @@
+package ch10.inheritance;
+
+public class Parent {
+//    public  Parent(){
+//        System.out.println("Parent.Parent");
+//    }
+
+    public void printName(){
+        System.out.println("Parent printName");
+    }
+
+}
