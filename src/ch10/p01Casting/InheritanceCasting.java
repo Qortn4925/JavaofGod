b7e5ad494512ab08fb1e0ae270286a9fa8c3b666
qortn4925 <qortn4925@naver.com>
2024-09-05 17:48:38 +0900

    형변환

--- src/ch10/p01Casting/InheritanceCasting.java
+++ src/ch10/p01Casting/InheritanceCasting.java
@@ -12,7 +12,23 @@ public class InheritanceCasting {
         ParentCasting pa = new ParentCasting();
         ChildCasting ca = new ChildCasting();
 
-        ParentCasting pa2 = ca;
-        ChildCasting ca2 =(ChildCasting) pa;
+        // 오류 발생
+//        ParentCasting pa2 = ca;
+//        ChildCasting ca2 =(ChildCasting) pa;
+
+        //이건 오류 안남 ,  생성시 부터  자식 객체를 받고 있던거라서
+//        ParentCasting pa2 = ca;
+//        ChildCasting ca2 =(ChildCasting) pa2;
+    }
+    public  void objectCasint2(){
+        ChildCasting ca = new ChildCasting();
+        ParentCasting pa = ca;
+        ChildCasting c2 = (ChildCasting) pa;
+    }
+    public void objectCastArray(){
+        ParentCasting[] parent = new ParentCasting[3];
+        parent[0]=new ChildCasting();
+        parent[1] = new ParentCasting();
+        parent[2] = new ChildCasting();
     }
 }
