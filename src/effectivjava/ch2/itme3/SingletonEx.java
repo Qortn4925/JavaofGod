847aa70601f70c111cd8969b9e10b197c0726fae
Qortn4925 <114649506+Qortn4925@users.noreply.github.com>
2025-09-11 17:32:06 +0900

    effective java: item3: enum을 통한 싱글톤 구현

---
+++ src/effectivjava/ch2/itme3/SingletonEx.java
@@ -0,0 +1,21 @@
+package effectivjava.ch2.itme3;
+
+public class SingletonEx {
+    // 1번 , private 생성자로 두고  멤버변수로 접근 ( 컴파일시 하나만 생성되는게 자명해짐 )
+
+//    public static final SingletonEx  singleton = new SingletonEx();
+
+    // 2번 멤버변수를 private 으로 두고 , 정적 팩토리 메소드를 통한 접근
+    private  static final SingletonEx singleton = new SingletonEx();
+
+    public  static SingletonEx getInstance() { return  singleton;}
+
+
+    private SingletonEx() {
+    }
+
+    private Object readResolve() {
+        return  singleton;
+    }
+
+}
---
+++ src/effectivjava/ch2/itme3/SingletonEx2.java
@@ -0,0 +1,36 @@
+package effectivjava.ch2.itme3;
+
+public enum SingletonEx2 {
+    INSTANCE;
+
+    public void leaveTheBuilding() {
+        System.out.println("기다려 금방 나갈겡");
+    }
+
+    private int count =0;
+
+    public void addOne() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        SingletonEx2 singletonEx2 = SingletonEx2.INSTANCE;
+        SingletonEx2 singletonEx1 = SingletonEx2.INSTANCE;
+
+        singletonEx1.addOne();
+        singletonEx2.addOne();
+
+        System.out.println("singletonEx1 = " + singletonEx1.getCount());
+
+        singletonEx2.leaveTheBuilding();
+        if(singletonEx1.getCount()==singletonEx2.getCount()){
+            System.out.println("일치");
+        }
+    }
+
+}
