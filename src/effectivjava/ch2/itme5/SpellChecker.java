6c50eca9fb3bf2319bb4354c4be72f0fe0cd1d02
Qortn4925 <114649506+Qortn4925@users.noreply.github.com>
2025-09-12 17:58:49 +0900

    effective java: item5 자원을 직적 명시하지 말고 의존 객체 주입을 사용하라

---
+++ src/effectivjava/ch2/itme5/SpellChecker.java
@@ -0,0 +1,45 @@
+package effectivjava.ch2.itme5;
+
+import java.util.List;
+import java.util.Objects;
+
+public class SpellChecker {
+
+    // 버전 정적 유틸리티를 잘못 사용한 예시
+    /*
+    private static final Lexicon dictionary = ;
+
+    private SpellChecker() {
+    }
+
+    public static boolean isValid(String word) {
+
+    }
+
+    public static List<String> suggestions(String type) {
+*/
+
+    // 2 싱글톤을 잘못 사용한 예
+    /*
+    private final Lexicon dictionary = ;
+
+    private SpellChecker(){}
+
+    public static SpellChecker INSTANCE = new SpellChecker();
+
+    public boolean isValid(){}
+
+    public List<String> suggestions(String type) {}
+*/
+
+//    private final Lexicon dictionary;
+//
+//    public SpellChecker(Lexicon dictionary) {
+//        this.dictionary = Objects.requireNonNull(dictionary);
+//    }
+//
+//    public  boolean isValid(String word) {}
+//
+//    public List<String> suggestions(String type) {}
+}
+
