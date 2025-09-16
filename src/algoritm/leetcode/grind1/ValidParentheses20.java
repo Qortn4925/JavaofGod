f35b36bce00f3b8421a00af0bbf61f5f30b1a20e
Qortn4925 <114649506+Qortn4925@users.noreply.github.com>
2025-09-17 02:33:28 +0900

    leetcode : ValidParentheses20 다양하게 풀어보기

--- src/algoritm/leetcode/grind1/ValidParentheses20.java
+++ src/algoritm/leetcode/grind1/ValidParentheses20.java
@@ -30,5 +30,60 @@ public class ValidParentheses20 {
 
             return stack.isEmpty();
         }
+
+        //카운터 방식,, 가장 최근과 매칭되는 닫는게 필요해서 안됨.
+        public static  boolean isValid2(String s) {
+            int round = 0, square = 0, curly = 0;
+
+            // round , squer, curly
+            for(char a:s.toCharArray()) {
+                switch (a) {
+                    case '(':
+                        round++;
+                        break;
+                    case ')' :
+                        round--;
+                        break;
+                    case '{':
+                        curly++;
+                        break;
+                    case '}':
+                        curly--;
+                        break;
+                    case '[':
+                        square++;
+                        break;
+                    case ']':
+                        square--;
+                        break;
+                }
+                if(round<0 || curly <0 || square<0) {
+                    return false;
+                }
+                System.out.println("round = " + round+" squere = "+square+" culry ="+curly);
+            }
+
+//            if(round+square+curly>0) return  false;
+//            else  return  true;
+
+//            if((round + square + curly) == 0) return true;
+//            else return false;
+            if(round==0&square==0&&curly==0) return  true;
+            else  return false;
+        }
+
+        public static  boolean isValid3(String s) {
+
+            if(s.length() %2==1) return false;
+
+            if(s.isEmpty()) return  true;
+
+
+        }
+
+    }
+    public static void main(String[] args) {
+        String s ="()";
+        System.out.println("isValid2 = " + Solution.isValid2(s));
     }
 }
