71a7fbe953adf06c542d68e21c0976c6a3546309
Qortn4925 <114649506+Qortn4925@users.noreply.github.com>
2025-09-18 16:45:32 +0900

    effectiv java: item9 try-with-resource +catch

--- src/effectivjava/ch2/item9/Itme9Ex.java
+++ src/effectivjava/ch2/item9/Itme9Ex.java
@@ -46,5 +46,13 @@ public class Itme9Ex {
         }
     }
 
+    static String firstLineOfFile(String path, String defaultVal) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            return br.readLine();
+        } catch (IOException e) {
+            return  defaultVal;
+        }
+    }
+
 
 }
