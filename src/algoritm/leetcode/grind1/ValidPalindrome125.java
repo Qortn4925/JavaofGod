36e40bb66ca9b24a1936ee0acb5db07ac189f3b8
Qortn4925 <114649506+Qortn4925@users.noreply.github.com>
2025-09-23 03:23:27 +0900

    leetcode  ValidPalindrome125, Two Pointer

--- src/algoritm/leetcode/grind1/ValidPalindrome125.java
+++ src/algoritm/leetcode/grind1/ValidPalindrome125.java
@@ -23,6 +23,23 @@ public class ValidPalindrome125 {
         }
     }
 
-        public boolean
+        public boolean isPalindrome1 (String s) {
+         int left =0 , right=s.length()-1;
+
+            while (left < right) {
+                while(left<right && ! Character.isLetterOrDigit(s.charAt(left))) left ++;
+                while(left<right && ! Character.isLetterOrDigit(s.charAt(right))) right--;
+
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)) ) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+
+            return  true;
+
+        }
 
 }
