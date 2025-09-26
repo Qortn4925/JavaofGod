91cb08dff1a751689754012a3417fc84badef7af
Qortn4925 <114649506+Qortn4925@users.noreply.github.com>
2025-09-26 12:26:27 +0900

    leetcode: BinarySearch704

---
+++ src/algoritm/leetcode/grind1/BinarySearch704.java
@@ -0,0 +1,30 @@
+package algoritm.leetcode.grind1;
+
+public class BinarySearch704 {
+
+    public static int search(int[] nums , int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid; // target 찾음
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {-1,0,3,5,9,12} ;
+         int target =9;
+
+        int result = search(nums, target);
+        System.out.println("result = " + result);
+    }
+    }
