ed83c854211a40086655c15295d83bb557bdcaf3
qortn4925 <qortn4925@naver.com>
2026-01-10 17:21:19 +0900

    pss:Tree 21.3 postOrder

--- src/algoritm/strategy/tree21/Tree.java
+++ src/algoritm/strategy/tree21/Tree.java
@@ -25,4 +25,26 @@ class  a{
 
           return curheight;
      }
+     // index 찾아서 , 왼쪽 섭트리 그대로 출력 중앙 기준으로 뒤집어서 ,출력
+     int[] postOrder( int[] preOrder, int[] inOrder){
+          int root = preOrder[0];
+          int[] a= new int[preOrder.length];
+          a[0]=root;
+          int index;
+          for(int i=0; i<preOrder.length;i++){
+                    if(inOrder[i]==root){
+                         index =i;
+                    }
+          }
+
+          for(int i=0; i<inOrder.length; i++) {
+               if( i>0 && i<index) {
+                    System.out.println(preOrder[i]);
+               }else if( i>index){
+                    System.out.println(inOrder[preOrder.length]);
+               }
+          }
+         return a;
+     }
+
 }
