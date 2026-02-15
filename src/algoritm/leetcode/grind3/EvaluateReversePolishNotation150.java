8a80a9497f00f18f327641964a5d66b94a898c55
qortn4925 <qortn4925@naver.com>
2026-02-15 20:36:08 +0900

    leetcode:EvaluateReversePoilishNotation150

--- src/algoritm/leetcode/grind3/EvaluateReversePolishNotation150.java
+++ src/algoritm/leetcode/grind3/EvaluateReversePolishNotation150.java
@@ -1,12 +1,42 @@
 package algoritm.leetcode.grind3;
 
+import java.util.Stack;
+
 public class EvaluateReversePolishNotation150 {
 
 
     public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
 
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
 
+                case "-": {
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(a - b);
+                    break;
+                }
 
-        return 0;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+
+                case "/": {
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(a / b);
+                    break;
+                }
+
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
     }
 }
